d25a02dadb1c188606ad117fadd9706692fdbcab
Johnny_Ma <johnny880319@gmail.com>
2026-04-27 12:05:54 +0800

    codecrafters submit [skip ci]

--- cmd/my_kafka/main.go
+++ cmd/my_kafka/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net"
 	"os"
@@ -12,9 +13,44 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	defer func() {
+		if l.Close() != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}()
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			continue
+		}
+
+		go func(conn net.Conn) {
+			err := handleConnection(conn)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+		}(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) (err error) {
+	defer func() {
+		err = errors.Join(err, conn.Close())
+	}()
+
+	for {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if err != nil {
+			return fmt.Errorf("error reading from connection: %w", err)
+		}
+
+		_, err = conn.Write([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x07})
+		if err != nil {
+			return fmt.Errorf("error writing to connection: %w", err)
+		}
 	}
 }
